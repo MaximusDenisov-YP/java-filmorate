dbb90424525c274ae675feafd52ffe2e95abea59
Aleksey Tokarev <l00ser91@yande.ru>
2025-07-16 00:51:11 +0300

    тесты

--- src/test/java/ru/yandex/practicum/filmorate/FilmServiceIntegrationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmServiceIntegrationTest.java
@@ -1,18 +1,23 @@
 package ru.yandex.practicum.filmorate;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.director.DirectorStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.friendship.FriendshipStorage;
 import ru.yandex.practicum.filmorate.storage.genre.GenreDbStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmLikeDbStorage;
 import ru.yandex.practicum.filmorate.storage.mpa.MpaDbStorage;
+import ru.yandex.practicum.filmorate.storage.review.ReviewDbStorage;
+import ru.yandex.practicum.filmorate.storage.review.ReviewLikeDbStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
 import java.time.LocalDate;
@@ -20,20 +25,11 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@JdbcTest
+@SpringBootTest
 @AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-@Import({
-        FilmService.class,
-        FilmDbStorage.class,
-        UserDbStorage.class,
-        FilmLikeDbStorage.class,
-        MpaDbStorage.class,
-        GenreDbStorage.class
-})
 public class FilmServiceIntegrationTest {
-
-    private final FilmService filmService;
+    @Autowired
+    private FilmService filmService;
 
     @Test
     public void testCreateAndGetFilmById() {
